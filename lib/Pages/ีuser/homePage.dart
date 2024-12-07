c92d8208e502e58046cca8a6da6a140bd59da116
TawanPhiranan <65011212119@msu.ac.th>
2024-12-07 23:51:53 +0700

    UI Home page

--- lib/Pages/ีuser/homePage.dart
+++ lib/Pages/ีuser/homePage.dart
@@ -10,12 +10,41 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    final List<Map<String, dynamic>> dorms = [
+      {
+        'image': 'assets/images/bb.jpg',
+        'name': 'บุญญาดาเพลส',
+        'distance': '3 กม.',
+        'description':
+            'หอพักทันสมัยโมเดิร์น สะดวกครบครัน เฟอร์นิเจอร์บิวอิน บรรยากาศดี เป็นกันเอง',
+        'coordinates': 'พิกัดขามเรียง มมส.ใหม่'
+      },
+      {
+        'image': 'assets/images/Boss Place.jpg',
+        'name': 'หอพักหญิงบอสเพลส',
+        'distance': '3.5 กม.',
+        'description':
+            'หอพักหญิงบอสเพลส หอพักหญิง 100% : หอพักน่าอยู่ อบอุ่น เรียบหรูและมีความเป็นส่วนตัว มีรถโดยสารผ่าน',
+        'coordinates': 'พิกัดท่าขอนยาง ใกล้ มมส ใหม่'
+      },
+      {
+        'image': 'assets/images/Dragon Place.jpg',
+        'name': 'ดราก้อนเพลส',
+        'distance': '4 กม.',
+        'description':
+            'ดราก้อนเพลสห้องพักสวย สไตล์โมเดิร์น เฟอร์นิเจอร์ลงตัว น่าอยู่ มีทีวีและตู้เย็นทุกห้อง Internet แรง แยก Router',
+        'coordinates': ' พิกัดซอยวุ่นวาย ใกล้ (มมส.ใหม่)'
+      },
+    ];
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: const Text(
           'RoomMatch',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 28),
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 28),
         ),
         backgroundColor: const Color(0xff96B1E1),
         shape: const RoundedRectangleBorder(
@@ -25,7 +54,98 @@ class _HomepageState extends State<Homepage> {
           ),
         ),
       ),
-      body: Text('home'),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
+        child: Column(
+          children: [
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(
+                  Icons.location_on_sharp,
+                  color: Colors.red,
+                  size: 35,
+                ),
+                Text(
+                  'หอพักแนะนำ',
+                  style: TextStyle(
+                      color: Color(0xff489EE6),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: dorms.length,
+                itemBuilder: (context, index) {
+                  final dorm = dorms[index];
+                  return Card(
+                    margin: const EdgeInsets.symmetric(vertical: 10.0),
+                    color: Colors.grey[100],
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          dorm['image'],
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                          height: 240.0,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    dorm['name'],
+                                    style: const TextStyle(
+                                        fontSize: 18.0,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black),
+                                  ),
+                                  Text(
+                                    dorm['distance'],
+                                    style: const TextStyle(
+                                      fontSize: 16.0,
+                                      color: Color(0xff3AA6FF),
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: screenHeight * 0.01),
+                              Text(
+                                dorm['description'],
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.black,
+                                ),
+                              ),
+                               SizedBox(height: screenHeight * 0.01),
+                              Text(
+                                dorm['coordinates'],
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
